d63066e190a34e0a862637e46add7a2439e56551
177nakano <t.nakano@tdog.jp>
2024-09-06 14:40:41 +0900

    commit

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import lisntener
+import extractText
 
 
 if __name__ == "__main__":
     logger = lisntener.CoordinateLogger()
-    print("Click and drag to select area. Press ESC to exit.")
+    print("select area.")
     logger.start()
-    coordinate = []
-    if hasattr(logger, 'coordinates'):
-        coordinate = [logger.coordinates['start'],logger.coordinates['end']]
+    coordinates = []
+    if hasattr(logger, 'coordinates'):#左上右下
+        """coordinates = {"left" : logger.coordinates["start"][0],
+                       "top" : logger.coordinates["start"][1],
+                       "right" : logger.coordinates["end"][0],
+                       "bottom" : logger.coordinates["end"][1]}"""
+        coordinates = logger.coordinates['start'] + logger.coordinates['end']
 
-    print(coordinate)
+    #text = extractText.extract_text(coordinates)
+    text = extractText.extract_text((100, 200, 400, 300))
+    print(f"text : {text}")
