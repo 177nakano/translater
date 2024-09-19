0ab93a75ce4e9852d6fd9056dfab3ace7eab7c00
177nakano <t.nakano@tdog.jp>
2024-09-19 16:02:49 +0900

    del comment

--- main.py
+++ main.py
@@ -8,11 +8,8 @@ if __name__ == "__main__":
     print("select area.")
     logger.start()
     coordinates = []
-    if hasattr(logger, 'coordinates'):#左上右下
-        """coordinates = {"left" : logger.coordinates["start"][0],
-                       "top" : logger.coordinates["start"][1],
-                       "right" : logger.coordinates["end"][0],
-                       "bottom" : logger.coordinates["end"][1]}"""
+    if hasattr(logger, 'coordinates'):
+
         coordinates = logger.coordinates['start'] + logger.coordinates['end']
 
     text = extractText.extract_text(coordinates)
